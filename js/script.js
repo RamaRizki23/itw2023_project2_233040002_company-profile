a2b24e03fd7fd72178c667dbcf6fd6f0d851dac5
RamaRizki23 <rama23rizki04@gmail.com>
2023-12-30 01:57:31 +0700

    commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const menuBar = document.querySelector('.hambuger');
+const menuNav = document.querySelector('.navbar');
+
+menuBar.addEventListener('click', () => {
+  menuBar.classList.toggle('is-active');
+});
+
+window.addEventListener("scroll", () => {
+   menuNav.classList.toggle{"nav-scroll-active", window.scrollY > 0};
+});
