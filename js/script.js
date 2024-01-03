90eedc54ea96bf09837641a0e1fa61c89f45b608
RamaRizki23 <rama23rizki04@gmail.com>
2024-01-03 16:55:56 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const menuBar = document.querySelector('.hamburger');
 const menuNav = document.querySelector('.navbar');
+const menuNavigation = document.querySelector('.menu');
 
 menuBar.addEventListener('click', () => {
   menuBar.classList.toggle('is-active');
+  menuNavigation.classList.toggle('menu-active');
 });
 
 window.addEventListener('scroll', function () {
@@ -11,4 +13,9 @@ window.addEventListener('scroll', function () {
   } else {
     menuNav.classList.remove('nav-scroll-active');
   }
+
+  windowPosition = window.scrollY > 200;
+  menuBar.classList.toggle('btn-active', windowPosition);
+  menuBar.classList.remove('is-active');
+  menuNavigation.classList.remove('menu-active');
 });
